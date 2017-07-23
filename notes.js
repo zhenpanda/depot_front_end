aa3f69dfa6245cc4fb1c18d0d6c9cb2a8f307642
zhenting zhou <zhentzhou@gmail.com>
2017-07-23 04:22:01 -0500

    provider ready

--- notes.js
+++ notes.js
@@ -1,3 +1,10 @@
 /*
 working web3 npm install web3@0.19.1
+
+truffle migrate --reset
+reset server eth
+
+copy over eth address
+restart eth test server
+
 */
--- src/component/provider/Provider.js
+++ src/component/provider/Provider.js
@@ -1,12 +1,70 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions/index';
-// import * as Web3 from 'web3';
+import toastr from 'toastr';
+
+import $ from 'jquery';
+require('materialize-css/dist/css/materialize.css');
+window.jQuery = require('jquery');
+window.$ = require('jquery');
+require('materialize-css/dist/js/materialize.js');
+require('materialize-css/js/init.js');
+
+
+import header from '../../../assets/truck_page/truck_header.png';
+import truckFace from '../../../assets/truck_page/truck_face.png';
+import truckTop from '../../../assets/truck_page/truck_top.png';
 
 var Web3 = require('web3');
 var web3 = new Web3('ws://localhost:8545');
 
-var abi = [
+var abi =  [
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "listOfVehicles",
+    "outputs": [
+      {
+        "name": "spaceAvailable",
+        "type": "uint256"
+      },
+      {
+        "name": "totalSpace",
+        "type": "uint256"
+      },
+      {
+        "name": "pricePerCubicFootPerHour",
+        "type": "uint256"
+      },
+      {
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "name": "beginningCity",
+        "type": "bytes32"
+      },
+      {
+        "name": "endingCity",
+        "type": "bytes32"
+      },
+      {
+        "name": "beginDate",
+        "type": "uint256"
+      },
+      {
+        "name": "endDate",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  },
   {
     "constant": true,
     "inputs": [],
@@ -75,6 +133,14 @@ var abi = [
       {
         "name": "endingCity",
         "type": "bytes32"
+      },
+      {
+        "name": "beginDate",
+        "type": "uint256"
+      },
+      {
+        "name": "endDate",
+        "type": "uint256"
       }
     ],
     "payable": false,
@@ -166,6 +232,27 @@ var abi = [
     "type": "function"
   },
   {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "addr",
+        "type": "address"
+      },
+      {
+        "name": "cubicFeet",
+        "type": "uint256"
+      },
+      {
+        "name": "amountOfHours",
+        "type": "uint256"
+      }
+    ],
+    "name": "purchaseVehicleSpace",
+    "outputs": [],
+    "payable": true,
+    "type": "function"
+  },
+  {
     "constant": true,
     "inputs": [],
     "name": "version",
@@ -173,6 +260,23 @@ var abi = [
       {
         "name": "",
         "type": "string"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "vehicleDates",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256[]"
+      },
+      {
+        "name": "",
+        "type": "uint256[]"
       }
     ],
     "payable": false,
@@ -238,6 +342,39 @@ var abi = [
     "constant": false,
     "inputs": [
       {
+        "name": "_cubicFeet",
+        "type": "uint256"
+      },
+      {
+        "name": "pricePerCubicFootPerHour",
+        "type": "uint256"
+      },
+      {
+        "name": "startingPosition",
+        "type": "bytes32"
+      },
+      {
+        "name": "endingPosition",
+        "type": "bytes32"
+      },
+      {
+        "name": "beginDate",
+        "type": "uint256"
+      },
+      {
+        "name": "endDate",
+        "type": "uint256"
+      }
+    ],
+    "name": "addVehicle",
+    "outputs": [],
+    "payable": false,
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
         "name": "_to",
         "type": "address"
       },
@@ -278,24 +415,74 @@ var abi = [
     "type": "function"
   },
   {
-    "constant": false,
+    "constant": true,
     "inputs": [
       {
-        "name": "addr",
+        "name": "city",
+        "type": "bytes32"
+      }
+    ],
+    "name": "vehiclesByCity",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256[]"
+      },
+      {
+        "name": "",
+        "type": "uint256[]"
+      },
+      {
+        "name": "",
+        "type": "uint256[]"
+      },
+      {
+        "name": "",
         "type": "address[]"
       },
       {
-        "name": "cubicFeet",
+        "name": "",
+        "type": "bytes32[]"
+      },
+      {
+        "name": "",
+        "type": "bytes32[]"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "vehicles",
+    "outputs": [
+      {
+        "name": "",
         "type": "uint256[]"
       },
       {
-        "name": "amountOfHours",
+        "name": "",
         "type": "uint256[]"
+      },
+      {
+        "name": "",
+        "type": "uint256[]"
+      },
+      {
+        "name": "",
+        "type": "address[]"
+      },
+      {
+        "name": "",
+        "type": "bytes32[]"
+      },
+      {
+        "name": "",
+        "type": "bytes32[]"
       }
     ],
-    "name": "purchaseWarehouseSpace",
-    "outputs": [],
-    "payable": true,
+    "payable": false,
     "type": "function"
   },
   {
@@ -374,7 +561,8 @@ var abi = [
     "type": "event"
   }
 ];
-var truffleAddress = '0x609755f2afd16979c4793fd0caa861976ab5fff2';
+// replace if truffle is migrate reset
+var truffleAddress = '0x5c5ef960561e6c7c8def3f46c6ec4f15da2eb2bd';
 
 class Provider extends Component {
   // INIT State
@@ -384,8 +572,19 @@ class Provider extends Component {
     let test = web3.eth.contract(abi).at(truffleAddress);
     this.state = test;
   }
-  componentWillMount() {
+  componentDidMount() {
+    $(window).scrollTop(0);
     console.log("loading component....");
+
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year,
+      today: 'Today',
+      clear: 'Clear',
+      close: 'Ok',
+      closeOnSelect: false // Close upon selecting a date,
+    });
+
   }
   getData() {
     console.log("trying to hit web3...");
@@ -403,26 +602,134 @@ class Provider extends Component {
       console.log(res);
     })
   }
+  grabData() {
+    let start = document.getElementById('start_city').value;
+    let end = document.getElementById('end_city').value;
+    let startDate = document.getElementById('start-date-input').value;
+    let endDate = document.getElementById('end-date-input').value;
+    let perCost = document.getElementById('per-cost').value;
+    let total = document.getElementById('price').value;
+    let startTimeStamp = new Date(startDate).getTime() / 1000;
+    let endTimeStamp = new Date(endDate).getTime() / 1000;
+    // console.log(start,end,startTimeStamp, endTimeStamp, perCost,total);
+    //
+    // debugger;
+    this.state.addVehicle(total, perCost, start, end, startTimeStamp, endTimeStamp, {gas:500000, from:web3.eth.coinbase}, (err, res) =>{
+      // console.log(err);
+      // debugger;
+      // console.log(res);
+      console.log("You made it home boy!");
+      console.log(res);
+      toastr.success('Service Created!');
+      toastr.success('Transaction hash: '+ res);
+    })
+    //uint256 _cubicFeet, uint pricePerCubicFootPerHour, bytes32 startingPosition, bytes32 endingPosition, uint beginDate, uint endDate
+  }
+
 
   render() {
     return (
-      <div className="home">
-        <div>
-          <p>This is the provider page</p>
-          <a onClick={() => {this.getData()}}>testCLick</a>
-          <br />
-          <a onClick={() => {this.postData()}}>postCLick</a>
+      <div className="provider-page">
+        <div className="z-depth-1 provider-header"><img className="my-image" src={ header } /></div>
+        <div className="row">
+          <div className="col s6">
+            <img className="z-depth-2 my-image" src={ truckFace } />
+          </div>
+          <div className="col s6">
+            <img className="z-depth-2 my-image" src={ truckTop } />
+          </div>
         </div>
+
+        <div className="card-content card amber accent-1">
+
+          <div className="row card-inside">
+          <form className="col s12">
+            <div className="row">
+              <div className="input-field col s6">
+              <i className="fa fa-car" aria-hidden="true"></i>
+                <i className="material-icons prefix"></i>
+                <label className="lable-text">
+                Start Point City</label>
+                <input id="start_city" type="text" className="validate" />
+              </div>
+              <div className="input-field col s6">
+              <i className="fa fa-map-marker" aria-hidden="true"></i>
+                <i className="material-icons prefix"></i>
+                <label className="lable-text">
+                End Point City</label>
+                <input id="end_city" type="tel" className="validate" />
+              </div>
+            </div>
+          </form>
+          <div className="dates  card-inside">
+          <div className="row">
+          <form className="col s12">
+          <div className="row">
+          <div className="start-date col s6">
+          <i className="fa fa-calendar-minus-o" aria-hidden="true"></i>
+          <input id="start-date-input" type="text" className="datepicker" />
+          <label className="lable-text">Start Date</label>
+          </div>
+          <div className="end-date col s6">
+          <i className="fa fa-calendar" aria-hidden="true"></i>
+          <input id="end-date-input" type="text" className="datepicker" />
+          <label className="lable-text">End Date</label>
+          </div>
+          </div>
+          </form>
+          </div>
+          </div>
+
+          </div>
+
+        </div>
+
+        <div className="card-content card light-blue lighten-4">
+          <div className="row card-inside">
+            <form className="col s12">
+              <div className="row">
+                <div className="input-field col s6">
+                <i className="fa fa-connectdevelop" aria-hidden="true"></i>
+                  <i className="material-icons prefix"></i>
+                  <label className="lable-text">
+                  Price Per Unit (ETH)</label>
+                  <input id="per-cost" type="text" className="validate" />
+                </div>
+                <div className="input-field col s6">
+                <i className="fa fa-truck" aria-hidden="true"></i>
+                  <i className="material-icons prefix"></i>
+                  <label className="lable-text">
+                  Total Space (Cubic foot)</label>
+                  <input id="price" type="tel" className="validate" />
+                </div>
+              </div>
+            </form>
+          </div>
+
+      </div>
+
+      <div className="submit-main">
+        <a className="waves-effect teal lighten-1 btn-large" onClick={()=>{this.grabData()}}>
+          <i className="fa fa-cubes" aria-hidden="true"></i>
+        </a>
+      </div>
+
       </div>
     );
   }
 }
-// <img src={ splash } />
 
 function mapStateToProps(state) {
   // debugger;
-  return { board: state.auth }
+  return { block: state.auth }
 }
 // {require('../../assets/splash_img.png')}
 export default connect(mapStateToProps, actions)(Provider);
 // <a className="waves-effect waves-light btn" onClick={() => this.getData()}>HIT ME</a>
+
+// <div>
+//   <p>This is the provider page</p>
+//   <a onClick={() => {this.getData()}}>testCLick</a>
+//   <br />
+//   <a onClick={() => {this.postData()}}>postCLick</a>
+// </div>
